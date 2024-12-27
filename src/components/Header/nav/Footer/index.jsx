4106d269736581481dbe3e9459cb5a0a3fe4218e
David <garxia0710@gmail.com>
2024-12-27 14:58:54 -0500

    fix: menu footer hrefs updated

--- src/components/Header/nav/Footer/index.jsx
+++ src/components/Header/nav/Footer/index.jsx
@@ -3,10 +3,9 @@ import styles from './style.module.scss';
 export default function index() {
   return (
     <div className={styles.footer}>
-        <a>Awwwards</a>
-        <a>Instagram</a>
-        <a>Dribble</a>
-        <a>LinkedIn</a>
+        <a href='https://www.instagram.com/davidgarciamz/'>Instagram</a>
+        <a href='https://www.linkedin.com/in/davidgarciamu%C3%B1oz/'>LinkedIn</a>
+        <a href='https://github.com/davidgarciamunozz'>GitHub</a>
     </div>
   )
 }
